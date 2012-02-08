ee0c01b54f346b3f8faea30166a9b260227ed1cd
Hannah Hasken <hasken1@illinois.edu>
2012-02-07 20:28:42 -0600

    Changed to printStdOut and added l for sendCommand, verifyCommand, and promptForBaud/Serial; Renamed promptForSerial to promptForPort; Standardized previous comments; Comments added for verifyCommand and sendReadCommand; Changed valid opmodes for setOpmode.

--- ServoStar.py
+++ ServoStar.py
@@ -1,14 +1,16 @@
-'''
-Copyright 2012 Stanton T. Cady
-		
-ServoStart_python  v0.1 -- February 02, 2012
-		
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+"""
+    Copyright 2012 Stanton T. Cady
+    Copyright 2012 Hannah Hasken
+    
+    ServoStar_python  v0.1.1 -- February 07, 2012
+    
+    This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 	
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
-'''
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+"""
 
 import sys
 import serial
@@ -20,7 +22,7 @@ import _winreg as winreg
 import msvcrt
 import os
 import math
-        
+
 global quiet
 quiet = False
 global crLast
@@ -42,46 +44,79 @@ cutOutSpeed = 1350
 global torqueLimit
 torqueLimit = 6
 
-def sendCommand(ser,command):
+def sendCommand(l,ser,command):
+    """ 
+        Send serial command to dyno.
+        
+        Arguments:
+        ser -- the serial port object to use
+        command -- a string containing the command to be sent
+        
+        Returns:
+        True -- everything worked
+        False -- otherwise
+        
+    """
 	command = command + chr(0x0D)
 	try:
 		if(ser.isOpen()):
-			ser.flushOutput						# clear the serial output buffer
+            # Clear the serial output buffer.
+			ser.flushOutput
 			ser.flushInput
-			ser.read(ser.inWaiting())			# clear anything that is in waiting
-			for char in command:				# do for each character of command string
-				if(ser.write(char) > 0):		# send character out serial port
+            # Clear anything that is in waiting.
+			ser.read(ser.inWaiting())
+            # Do for each character of command string.
+			for char in command:
+                # Send character out serial port.
+				if(ser.write(char) > 0):
 					try:
+                        # Read a byte from the serial port.
 						c = ser.read()
-						if(c != char):		# read a byte from the serial port
-							print "Byte received (" + str(ord(c)) + ") does not match byte sent (" + str(ord(char)) + ")"
+						if(c != char):
+							printStdOut "Byte received (" + str(ord(c)) + ") does not match byte sent (" + str(ord(char)) + ")"
 							return False
 					except ser.SerialException:
-						print "Serial exception."
+						printStdOut "Serial exception."
 						return False
 				else:
-					print "Serial port error."
+					printStdOut "Serial port error."
 					return False
 			return True
 		else:
-			print "Serial connection not open."
+			printStdOut "Serial connection not open."
 	except AttributeError:
-		print "Serial connection never opened."
+		printStdOut "Serial connection never opened."
 	return False
-	
-def verifyCommand(ser):
+
+def verifyCommand(l,ser):
+    """ 
+        Verify serial command to dyno.
+        
+        Arguments:
+        ser -- the serial port object to use
+        
+        Returns:
+        True -- everything worked
+        False -- serial connection never opened
+        int(val) -- err number if an error occurred
+        
+    """
 	try:
 		if(ser.isOpen()):
+            # Check for new line character.
 			if(ser.read() == chr(0x0A)):
+                # Read a byte from the serial port.
 				c = ser.read()
 				if(c == '-'):
 					if(ser.read() == '-'):
 						if(ser.read() == '>'):
 							return True
+                # Check for bell character.
 				elif(c == chr(0x07)):
 					val = str()
 					while True:
 						b = ser.read()
+                        # Check for carriage return character.
 						if(b != chr(0x0D)):
 							val = val + b
 						else:
@@ -90,28 +125,43 @@ def verifyCommand(ser):
 									if(ser.read() == '-'):
 										if(ser.read() == '>'):
 											if val[:3] == 'ERR':
-												print val
-												# return err number if there is an error
+												printStdOut val
+												# Return err number if there is an error.
 												return int(val[4:6])
 											break
 		else:
-			print "Serial connection not open."
+			printStdOut "Serial connection not open."
 	except AttributeError:
-		print "Serial connection never opened."
+	printStdOut "Serial connection never opened."
 	return False
-	
+
 def sendWriteCommand(l,ser,command):
 	if(sendCommand(l,ser,command)):
 		return verifyCommand(l,ser)
-		
+
 def sendReadCommand(l,ser,command):
+    """ 
+        Send read command to dyno.
+        
+        Arguments:
+        ser -- the serial port object to use
+        command -- a string containing the command to be sent
+        
+        Returns:
+        False -- serial connection never opened
+        val -- the string that was read
+        
+    """
 	try:
 		if(ser.isOpen()):
 			if(sendCommand(l,ser,command)):
+                # Check if new line character.
 				if(ser.read() == chr(0x0A)):
 					val = str()
+                    # Read a byte from the serial port until a carriage return is read.
 					while True:
 						b = ser.read()
+                        # Check if carriage return character.
 						if(b != chr(0x0D)):
 							val = val + b
 						else:
@@ -139,24 +189,24 @@ def driveOk(l,ser):
 	if(int(rsp) == 1):
 		return True
 	return rsp
-	
+
 def driveReady(l,ser):
 	rsp = sendReadCommand(l,ser,'ready')
 	if(int(rsp) == 1):
 		return True
 	return rsp
-	
+
 def getVelocity(l,ser):
 	return int(sendReadCommand(l,ser,'v'))
-	
+
 def getCurrent(l,ser):
 	return sendReadCommand(l,ser,'i')
-	
+
 def getTorque(l,ser):
 	global currentScalingFactor
 	global torqueConstant
 	return float(getCurrent(l,ser))/(currentScalingFactor*torqueConstant)
-		
+
 def enableDrive(l,ser):
 	rsp = sendWriteCommand(l,ser,'en')
 	if(rsp == True):
@@ -172,9 +222,9 @@ def disableDrive(l,ser):
 		return True
 	printStdOut(l,"There was an error disabling the drive.")
 	return rsp
-		
+
 def setOpmode(l,ser,mode):
-	if(mode == 0 or mode == 2):
+	if(mode == 1 or mode == 2):
 		rsp = sendWriteCommand(l,ser,'opmode=' + str(mode))
 		if(rsp == True):
 			printStdOut(l,"Opmode changed successfully.")
@@ -183,14 +233,14 @@ def setOpmode(l,ser,mode):
 			printStdOut,("There was an error setting the opmode.")
 			return rsp
 	else:
-		printStdOut(l,"Valid opmodes are 0 or 2.")
+		printStdOut(l,"Valid opmodes are 1 or 2.")
 	return False
-	
+
 def setVelocity(l,ser,velocity):
 	if dynoMode == 1:
 		try:
 			if (float(velocity) >= 0 or float(velocity) <= 1300):
-				rsp = sendWriteCommand(l,ser,'j=' + str(velocity))
+				rsp = sendWriteCommand(l,ser,'v=' + str(velocity))
 				if(rsp == True):
 					printStdOut(l,"Velocity command sent successfully.")
 					return True
@@ -201,15 +251,17 @@ def setVelocity(l,ser,velocity):
 				printStdOut(l,"Velocity must be between 0 and 1300 RPM.")
 		except ValueError:
 			printStdOut(l,"Invalid velocity command.")
+    else:
+        printStdOut(l,"Dynamometer is in an unknown mode.")
 	return False
-	
+
 def setTorque(l,ser,torque):
 	global currentScalingFactor
 	global torqueConstant
 	if dynoMode == 2:
 		try:
 			if(float(torque) >= 0 or float(torque) <= 6):
-				# the drive torque command is actually a current command
+				# Convert to current as the drive torque command is actually a current command.
 				i = str(int(round(float(torque)*currentScalingFactor*torqueConstant,0)))
 				rsp = sendWriteCommand(l,ser,'t=' + i)
 				if(rsp == True):
@@ -224,16 +276,16 @@ def setTorque(l,ser,torque):
 	else:
 		printStdOut(l,"Dynamometer is in an unknown mode.")
 	return False
-	
+
 def changeTorque(l, f, ser, t, v, msg = ""):
 	global torqueLimit
 	if t > 0:
-		# attempt to change torque command
+		# Attempt to change torque command.
 		if setTorque(l,ser,t) == True:
-			# print speed and torque command to stdout
+			# Print speed and torque command to stdout.
 			printStdOut(l,"Speed: %i rpm; Te: %0.2f Nm; Tm: %0.2f Nm" % (v,t,t-round(computeLossTorque(v),2)),True)
-			# log torque command and velocity
-			# parameters: lock, file, torque, velocity
+			# Log torque command and velocity.
+			# Parameters: lock, file, torque, velocity.
 			logTorqueVelocity(l,f,t,v,msg)
 			return True
 		else:
@@ -241,11 +293,11 @@ def changeTorque(l, f, ser, t, v, msg = ""):
 	else:
 		printStdOut(l,"Negative torques cannot be applied right now.")
 	return False
-	
+
 def computeLossTorque(v):
 	global torqueConstant
 	return computeLossCurrent(v)/torqueConstant;
-	
+
 def computeLossCurrent(v):
 	global constantCoefficent
 	global linearCoefficient
@@ -266,7 +318,7 @@ def setTorqueLimit(l,ser,limit):
 		return True
 	printStdOut(l,"There was an error setting the torque limit.")
 	return rsp
-	
+
 def openSerial(l,port,baud):
 	printStdOut(l,"Connecting...")
 	ser = serial.Serial(int(port),long(baud),timeout=5)
@@ -303,41 +355,41 @@ def scanSerial():
 			break
 	return i-1
 
-def promptForSerial():
-	print "Serial ports available:"
+def promptForPort(l):
+	printStdOut "Serial ports available:"
 	numPorts = scanSerial()
 	port = raw_input("Select a serial port number: ")
 	if port == "q":
 		sys.exit()
 	elif (int(port) < 0):
-		print "Invalid port selected. Try again (or enter q to quit)."
-		return promptSorSerial()
+		printStdOut "Invalid port selected. Try again (or enter q to quit)."
+		return promptForPort()
 	else:
 		port = port.strip()
 		return port
 
-def promptForBaud(device):
+def promptForBaud(l,device):
 	if device == "dyno":
 		availableBaud = ['9600','19200']
 	elif device == "arduino":
 		availableBaud = ['9600','19200','28800','38400','57600','115200']
 	else:
 		availableBaud = ['9600']
-	print "Baud rates available:"
+	printStdOut "Baud rates available:"
 	for baud in availableBaud:
-		print baud
+		printStdOut baud
 	baud = raw_input("Select a baud rate: ")
 	if baud == "q":
 		sys.exit()
 	elif not baud in availableBaud:
-		print "Inavlid baud rate.  Try again (or enter q to quit)."
+		printStdOut "Inavlid baud rate.  Try again (or enter q to quit)."
 		return promptForBaud()
 	else:
 		return baud
 
 def killSystem(l, dyno = None, arduino = None, exit = True):
 	if dyno != None and dyno.isOpen():
-		# attempt to disable drive until successful
+		# Attempt to disable drive until successful.
 		try:
 			while disableDrive(l,dyno) != True:
 				time.sleep(0.1)
@@ -351,22 +403,23 @@ def killSystem(l, dyno = None, arduino = None, exit = True):
 
 def enableDyno(l, ser, mode = -1, torqueLimit = 6, testTorque = True):
 	global dynoMode
-	# if no mode is given, ask for one
+	# Ask for mode if none is given.
 	if mode == -1:
 		l.acquire()
 		mode = raw_input("Which mode should the dyno be placed in (1) velocity mode or (2) torque mode? ")
 		l.release()
 		mode = int(mode)
 	if mode == 1:
+        # Attempt to enable velocity mode.
 		return enableVelocityMode(l,ser)
 	elif mode == 2:
-		# attempt to enable torque mode
+		# Attempt to enable torque mode.
 		rsp = enableTorqueMode(l,ser,torqueLimit,testTorque)
 		if rsp == True:
 			dynoMode = 2
-		# look for easily fixable errors
+		# Look for easily fixable errors.
 		elif rsp == 5:
-			# a response code of 5 can usually be fixed by trying again
+			# A response code of 5 can usually be fixed by trying again.
 			time.sleep(0.1)
 			return enableDyno(l,ser,mode,torqueLimit,testTorque)
 		elif rsp == 23:
@@ -376,7 +429,7 @@ def enableDyno(l, ser, mode = -1, torqueLimit = 6, testTorque = True):
 	else:
 		printStdOut(l,"Inavlid mode.")
 	return False
-	
+
 def enableVelocityMode(l,ser):
 	rsp = disableDrive(l,ser)
 	if(rsp == True):
@@ -386,7 +439,7 @@ def enableVelocityMode(l,ser):
 			time.sleep(0.1)
 			rsp = enableDrive(l,ser)
 			if(rsp == True):
-				# change global variable storing dyno mode to 1 (velocity mode)
+				# Change global variable storing dyno mode to 1 (velocity mode).
 				global dynoMode
 				dynoMode = 1
 				testVelocityMode = raw_input("Dynamometer set to velocity mode.  Shall I test the drive? (y/n) ")
@@ -404,7 +457,7 @@ def enableVelocityMode(l,ser):
 				return True
 	printStdOut(l,"There was an error enabling velocity mode.")
 	return False
-	
+
 def enableTorqueMode(l,ser, torqueLimit = None, testTorque = True):
 	rsp = disableDrive(l,ser)
 	if(rsp == True):
@@ -432,7 +485,7 @@ def enableTorqueMode(l,ser, torqueLimit = None, testTorque = True):
 					rsp = "Active response: " + str(active) + "; Driveok response: " + str(driveok)
 	printStdOut(l,"There was an error enabling torque mode.")
 	return rsp
-	
+
 def testTorqueMode(l,ser, t = None):
 	if t == None:
 		t = raw_input(" What torque would you like to apply for the test? ")
@@ -456,31 +509,31 @@ def checkSpeed(l,f,ser,va,t):
 		va.value = int(v)
 		logTorqueVelocity(l,f,t,v)
 		return v
-	
+
 def setupDynoSerial(l, port = None, baud = None):
-	# check if parameters for serial port and baud rate have been passed in
+	# Check if parameters for serial port and baud rate have been passed in.
 	if port == None or baud == None:
-		# missing port or baud so ask for them
+		# Missing port or baud so ask for them.
 		print "Attempting to setup the dynamometer."
 		q = raw_input("Shall I attempt to connect to the drive using default settings? (y/n): ")
 		if port == None:
 			if q == 'y':
 				port = 0
 			else:
-				port = promptForSerial()
+				port = promptForPort()
 		if baud == None:
 			if q == 'y':
 				baud = 9600
 			else:
-				baud = promptForBaud("dyno")
+				baud = promptForBaud(l,"dyno")
 	printStdOut(l,"Attempting to setup the dynamometer.")
-	# attempt to open a serial connection to the dyno
+	# Attempt to open a serial connection to the dyno.
 	return openSerial(l,port,baud)
 
 def startDyno(l, f, ser, va, initialTorque = None, vref = None, initialDelay = None):
 	if vref == None:
 		vref = 1000
-		#initialSpeed = int(raw_input("What velocity in rpm would like to use? "))
+    #initialSpeed = int(raw_input("What velocity in rpm would like to use? "))
 	if vref < 1300:
 		printStdOut(l,"Starting dyno to " + str(vref) + " rpm.")
 		if initialTorque == None:
@@ -502,7 +555,7 @@ def startDyno(l, f, ser, va, initialTorque = None, vref = None, initialDelay = N
 				except KeyboardInterrupt:
 					killSystem(l,ser)
 	else:
-		print "Please choose a speed below 1300 rpm."
+		printStdOut "Please choose a speed below 1300 rpm."
 		startDyno(l,ser)
 
 def printStdOut(l, msg, cr = False):
